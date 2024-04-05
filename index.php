ee066c38092198ecf2d31e616cde6f0fefcdf14b
Mi Peter <125556028+MiPeeter@users.noreply.github.com>
2024-04-05 05:44:14 -0400

    hero changes

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
-<?php include('includes/header.php');?>
+<?php include('includes/header.php'); ?>
+<div class="welcome-area pt-120">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-sm-10 col-md-6">
+                <div class="welcome-content mb-5 mb-md-0">
+                    <h2 data-aos="fade-up" data-aos-duration="750" data-aos-delay="500">Build an excellent NFTs website with Funto.</h2>
 
-<?php include('includes/footer.php');?>
+
+                    <div class="hero-btn-group">
+                        <div class="newsletter-form mt-5 me-5">
+                            <form class="d-flex align-items-stretch" action="#">
+                                <input class="form-control" type="email" placeholder="Enter email">
+                                <button class="btn btn-warning px-3" type="submit">Join Us <i class="bi bi-arrow-right"></i></button>
+                            </form>
+                        </div>
+                        <div class="social">
+                            <a href="javascript:;"><i class="bi bi-twitter"></i></a>
+                            <a href="javascript:;"><i class="bi bi-send"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-9 col-md-6">
+                <div class="welcome-thumb" data-aos="fade-up" data-aos-duration="750" data-aos-delay="500"><img src="img/illustrator/2.png" alt=""></div>
+            </div>
+            <div class="row">
+                <div class="col-lg"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="divider"></div>
+<div class="container">
+    <div class="border-top"></div>
+</div>
+<?php include('includes/footer.php'); ?>
